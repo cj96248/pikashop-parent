04a90ca0e93ac25c277ea933476eb8bd4bf4cf31
cj96248 <cj96248@sina.com>
2019-06-14 23:22:06 +0800

    数据访问整体框架

--- pikashop-dao/src/test/java/com/chao/AppTest.java
+++ pikashop-dao/src/test/java/com/chao/AppTest.java
@@ -1,20 +1,20 @@
 package com.chao;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Unit test for simple App.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext-*.xml"})
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test(){
+
     }
 }
---
+++ pikashop-management/src/main/java/com/chao/controller/BrandController.java
@@ -0,0 +1,23 @@
+package com.chao.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.chao.pojo.BrandDo;
+import com.chao.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/brand")
+public class BrandController {
+
+    @Reference
+    private BrandService brandService;
+
+    @RequestMapping("/list")
+    public List<BrandDo> findAll(){
+        return brandService.listBrand();
+    }
+}

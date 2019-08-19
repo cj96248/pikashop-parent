4f57c46baaa7ee16f03002be70effba58150500d
cj96248 <cj96248@sina.com>
2019-08-19 22:47:18 +0800

    添加新服务

---
+++ pikashop-basic-interface/src/main/java/com/chao/website/service/SearchService.java
@@ -0,0 +1,12 @@
+package com.chao.website.service;
+
+import com.chao.common.viewobject.CommonResult;
+import com.chao.mybatis.pojo.ItemDo;
+
+/**
+ * 搜索服务接口
+ */
+public interface SearchService {
+
+    CommonResult searchItem();
+}

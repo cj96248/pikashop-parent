0fe4ebecb19b27f3a652863a20af39893ca42e80
mapull <mapull@126.com>
2020-02-11 21:59:21 +0800

    Content category code refactor

---
+++ pikashop-basic-entity/src/main/java/com/chao/mybatis/website/pojo/ContentCategoryDo.java
@@ -0,0 +1,25 @@
+package com.chao.mybatis.website.pojo;
+
+import java.io.Serializable;
+
+public class ContentCategoryDo implements Serializable {
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+}
